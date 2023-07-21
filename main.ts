bcf316bed99ab2c479a99ce7cec05f20f946c947
antonios-bazigos <140197520+antonios-bazigos@users.noreply.github.com>
2023-07-21 19:40:33 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.West)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showArrow(ArrowNames.East)
+})
+basic.showArrow(ArrowNames.North)
+basic.forever(function () {
+	
+})
